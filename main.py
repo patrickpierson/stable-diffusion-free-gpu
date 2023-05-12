4c9c4f9947c5718404a5116629c694742cb760a1
Patrick Pierson <patrickpierson@users.noreply.github.com>
2023-05-12 17:48:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import base64
 from io import BytesIO
 
 # Load model
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small").to("cuda")
+#tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+#model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small").to("cuda")
 
 pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
 pipe.to("cuda")
@@ -39,9 +39,10 @@ def generate():
   img_str = "data:image/png;base64," + str(img_str)[2:-1]
 
   #generate text
-  input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
-  generated_output = model.generate(input_ids, do_sample=True, temperature=1.0, max_length=2500, num_return_sequences=1)
-  generated_text = tokenizer.decode(generated_output[0], skip_special_tokens=True)
+  #input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+  #generated_output = model.generate(input_ids, do_sample=True, temperature=1.0, max_length=2500, num_return_sequences=1)
+  #generated_text = tokenizer.decode(generated_output[0], skip_special_tokens=True)
+  generated_text = "disabled"
 
   print("Sending image and text ...")
  
